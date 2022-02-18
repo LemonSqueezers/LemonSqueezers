fd972dffeb823e2821569a82380d6abb384f40b8
Trololo <90335574+FrankEnderman@users.noreply.github.com>
2022-02-18 17:53:44 +0530

    Create Reader.bat

---
+++ Reader.bat
@@ -0,0 +1,10 @@
+@echo off
+title Lemon Squeezers Journal
+goto content
+:content
+curl https://raw.githubusercontent.com/FrankEnderman/LemonSqueezers/main/NEWS.md
+echo.
+echo.
+echo Press any key to reload
+pause
+goto content
