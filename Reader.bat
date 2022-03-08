7327140e373af501986b9f6905d68f2e639ca012
Trololo <90335574+FrankEnderman@users.noreply.github.com>
2022-03-08 03:23:48 +0000

    fix URL

--- Reader.bat
+++ Reader.bat
@@ -3,7 +3,7 @@ title Lemon Squeezers Journal
 goto content
 :content
 cls
-curl https://raw.githubusercontent.com/FrankEnderman/LemonSqueezers/main/NEWS.md
+curl https://raw.githubusercontent.com/LemonSqueezers/LemonSqueezers/main/NEWS.md
 echo.
 echo.
 echo Press any key to reload
